87e0df5576ee85db36232b37ad63de8accf42e0d
Ewan Porter <porter.ewan@mail.com>
2022-08-19 10:14:40 +0100

    please add

--- server/routes/LoginRoutes.js
+++ server/routes/LoginRoutes.js
@@ -64,4 +64,18 @@ router.get('/isAuth', async (req, res) => {
   }
 });
 
+router.delete('/logout', async (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.status(400).send('Unable to log out');
+      } else {
+        res.send('User Logged Out');
+      }
+    });
+  } else {
+    res.end();
+  }
+});
+
 module.exports = router;
